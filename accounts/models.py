718cafcec661ce32bb50681525811de8490db865
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-11-18 14:42:43 +0545

    ..

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import random
+import string
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+from django.db import models
