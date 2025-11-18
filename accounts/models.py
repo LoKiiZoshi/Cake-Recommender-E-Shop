28c2469dd645f5721297fec7044aa68f2bd46ba7
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-11-18 15:08:00 +0545

    change

--- accounts/models.py
+++ accounts/models.py
@@ -17,3 +17,15 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Profile"
 from django.db import models
+
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+from django.db import models
