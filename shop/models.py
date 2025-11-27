2e7d4553a135c54da84a2f2fe94ec9dbcc15231c
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-11-27 21:54:58 +0545

    some product model arccreate

--- shop/models.py
+++ shop/models.py
@@ -26,3 +26,16 @@ class Category(models.Model):
         return reverse('shop:product_list_by_category',args=[self.slug])
     
     
+    
+class Product(models.Model): 
+    category = models.ForeignKey(Category,related_name = 'products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length='product/%Y/%m/%d', blank=True)
+    description = models.TextField(blank= True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+    
