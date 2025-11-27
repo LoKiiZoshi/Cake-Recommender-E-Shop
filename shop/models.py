345247d2f51e3bed983696fa0e47cac41eedb03e
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-11-27 21:48:34 +0545

    create category model

--- shop/models.py
+++ shop/models.py
@@ -1,18 +1,28 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User 
+from django.urls import reverse
+from django.utils import timezone
+from django.utils import timezone
+import uuid
 
-# Create your models here.
-from django.db import models
 
-# Create your models here.
-from django.db import models
 
-# Create your models here.
-from django.db import models
-
-# Create your models here.
-from django.db import models
-
-# Create your models here.
-from django.db import models
-
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100,unique=True)
+    description = models.TextField(blank=True)
+    
+    
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        
+    def __str__(self):
+        return self.name
+    
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',args=[self.slug])
+    
+    
