b8bb8fb7a549a089a749cfd4500a42e077206a3d
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-12-03 23:11:13 +0545

    change

--- shop/recommendation.py
+++ shop/recommendation.py
@@ -20,31 +20,6 @@ def get_recommendations(user, method = 'hybrid', product = None,limit = 5):
     
    """
    
-   
-
-    if method == 'collaborative':
-        return collaborative_filtering(user, limit)
-    elif method == 'content':
-        return content_based_filtering(product, limit)
-    elif method == 'clustering':
-        return clustering_recommendations(user, limit)
-    elif method == 'clean':
-        return clean_recommendations(user, limit)
-    else:  # hybrid (default)
-        collab_recs = collaborative_filtering(user, limit)
-        
-        # If we have a specific product, get content-based recommendations too
-        
-        if product:
-            content_recs = content_based_filtering(product, limit)
-            
-            # Combine and deduplicate recommendations
-            
-            hybrid_recs = list(collab_recs)
-            for rec in content_recs:
-                if rec not in hybrid_recs:
-                    hybrid_recs.append(rec)
-            return hybrid_recs[:limit]
         
         return collab_recs
     
